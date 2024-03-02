025195645dd4baa39efd0f2d5ff28632e57469b2
PurobiParomita <142655040+PurobiParomita@users.noreply.github.com>
2024-03-02 23:34:10 +0600

    Add files via upload

---
+++ registration/index.php
@@ -0,0 +1,31 @@
+<?php
+    include("connection.php");
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+      <div id="form">
+            <h1 id="heading">Registration Form</h1><br>
+            <form name="form" action="signup.php" method="POST">
+                <i class="fa-regular fa-user"></i>
+                <input type="text" id="user" name="user" placeholder="Enter Username" required></br></br>
+                <i class="fa-regular fa-envelope"></i>
+                <input type="email" id="email" name="email" placeholder="Enter Email" required></br></br>
+                <i class="fa-solid fa-key"></i>
+                <input type="password" id="pass" name="pass" placeholder="Create Password" required></br></br>
+                <i class="fa-solid fa-key"></i>
+                <input type="password" id="cpass" name="cpass" placeholder="Confirm Password" required></br></br>
+                <input type="submit" id="btn" value="SignUp" name = "submit"/>
+            </form>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
