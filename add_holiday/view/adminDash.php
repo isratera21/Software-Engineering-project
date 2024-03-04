667f3913b18ab1c2ac22262598fe480e9f3f8c82
Israt Zahan Era <161957265+isratera21@users.noreply.github.com>
2024-03-04 07:04:11 +0600

    Add files via upload

---
+++ add_holiday/view/adminDash.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            color: #333;
+            margin-top: 20px;
+            text-align: center;
+        }
+        h3 {
+            
+            margin-left: 20px;
+            
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+            text-align: center;
+        }
+
+        li {
+            margin-bottom: 10px;
+            display: inline-block;
+        }
+
+        a {
+            text-decoration: none;
+            color: #007bff;
+            transition: color 0.3s ease;
+            padding: 10px 20px;
+            border: 1px solid #007bff;
+            border-radius: 5px;
+            background-color: #fff;
+        }
+
+        a:hover {
+            color: #f7f7f7;
+            background-color: #1977ad;
+        }
+    </style>
+</head>
+<body>
+    <h3><a href="../index.html">Back</a></h3>
+    <h1>Admin Dashboard</h1>
+    <ul>
+        <li><a href="addHoliday.php">Add Holiday</a></li><br><br>
+        <li><a href="viewHoliday.php">View Holidays</a></li><br><br>
+        <li><a href="viewBookedEvent.php">View Booked Event</a></li>
+    </ul>
+</body>
+</html>
